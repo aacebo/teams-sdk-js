36a2527e3ea615e5d3b0f6291c5980dd3a64f332
Alex Acebo <aacebowork@gmail.com>
2024-08-14 15:00:52 -0700

    cleanup botbuilder sample

--- samples/botbuilder/src/index.ts
+++ samples/botbuilder/src/index.ts
@@ -6,19 +6,26 @@ import {
   ConfigurationServiceClientCredentialFactory,
 } from 'botbuilder-core';
 
+const clientId = process.env.CLIENT_ID || 'b4e3dcad-6c1a-4f21-8a48-dd539afa61bb';
+const clientSecret = process.env.CLIENT_SECRET || 'C4y8Q~d_Ip-wdR4pcLByptK2.Z.xg51ialgDtbyb';
+const logger = new ConsoleLogger({
+  level: 'debug',
+  name: '@samples/botbuilder',
+});
+
 const app = new App({
   type: 'MultiTenant',
-  clientId: process.env.CLIENT_ID || 'b4e3dcad-6c1a-4f21-8a48-dd539afa61bb',
-  clientSecret: process.env.CLIENT_SECRET || 'C4y8Q~d_Ip-wdR4pcLByptK2.Z.xg51ialgDtbyb',
-  logger: new ConsoleLogger({ level: 'debug', name: '@samples/botbuilder' }),
+  clientId,
+  clientSecret,
+  logger,
   receiver: new TeamsAdapter({
+    logger,
     auth: new ConfigurationBotFrameworkAuthentication(
       {},
       new ConfigurationServiceClientCredentialFactory({
         MicrosoftAppType: 'MultiTenant',
-        MicrosoftAppId: process.env.CLIENT_ID || 'b4e3dcad-6c1a-4f21-8a48-dd539afa61bb',
-        MicrosoftAppPassword:
-          process.env.CLIENT_SECRET || 'C4y8Q~d_Ip-wdR4pcLByptK2.Z.xg51ialgDtbyb',
+        MicrosoftAppId: clientId,
+        MicrosoftAppPassword: clientSecret,
       })
     ),
   }),
