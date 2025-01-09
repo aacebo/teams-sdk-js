02bccb3d9e02fa86a925bdef68a8277ebc0b370d
Alex Acebo <aacebowork@gmail.com>
2025-01-09 09:06:55 -0800

    add tailwind

---
+++ packages/devtools/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx,css}'
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
