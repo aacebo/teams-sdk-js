bedb55585fe76a8733b653a20c9a1970dadf3ae4
aacebo <aacebowork@gmail.com>
2025-01-11 13:30:16 -0500

    add events

---
+++ packages/common/src/events/event-emitter.ts
@@ -0,0 +1,45 @@
+export type EventHandler<T = any> = (data: T) => void | Promise<void>;
+
+export class EventEmitter<EventTypes = Record<string, any>> {
+  protected index = -1;
+  protected subscriptions = new Map<keyof EventTypes, Array<{
+    readonly id: number;
+    readonly handler: EventHandler;
+  }>>();
+
+  on<Event extends keyof EventTypes>(event: Event, handler: EventHandler<EventTypes[Event]>) {
+    const id = ++this.index;
+    const subs = this.subscriptions.get(event) || [];
+    subs.push({ id, handler });
+    this.subscriptions.set(event, subs);
+    return id;
+  }
+
+  once<Event extends keyof EventTypes>(event: Event, handler: EventHandler<EventTypes[Event]>) {
+    const id = this.on(event, value => {
+      this.off(id);
+      handler(value);
+    });
+
+    return id;
+  }
+
+  off(id: number) {
+    for (const [_, subs] of this.subscriptions.entries()) {
+      const i = subs.findIndex(s => s.id === id);
+
+      if (i === -1) continue;
+
+      subs.splice(i, 1);
+      return;
+    }
+  }
+
+  emit<Event extends keyof EventTypes>(event: Event, value: EventTypes[Event]) {
+    const subs = this.subscriptions.get(event) || [];
+
+    for (const sub of subs) {
+      sub.handler(value);
+    }
+  }
+}
