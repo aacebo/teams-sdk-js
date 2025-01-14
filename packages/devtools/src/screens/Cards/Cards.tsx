f4d139db7496fb03d725ab188ba098eb3c3b4e68
Alex Acebo <aacebowork@gmail.com>
2025-01-14 09:38:55 -0800

    Update Cards.tsx

--- packages/devtools/src/screens/Cards/Cards.tsx
+++ packages/devtools/src/screens/Cards/Cards.tsx
@@ -1,5 +1,9 @@
 import './Cards.css';
 
 export default function Cards() {
-  return <div className="Cards">Cards...</div>;
+  return (
+    <div className="Cards">
+      Cards...
+    </div>
+  );
 }
