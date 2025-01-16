822b5eb0a3193ef2920c4559e7ac45746af7eabe
Alex Acebo <aacebowork@gmail.com>
2025-01-16 11:26:12 -0800

    Update ImageCard.tsx

--- packages/devtools/src/components/Card/Medias/ImageCard.tsx
+++ packages/devtools/src/components/Card/Medias/ImageCard.tsx
@@ -1,9 +1,21 @@
 import { Image } from '@teams.sdk/cards';
+import classNames from 'classnames';
 
 export interface ImageCardProps {
   readonly value: Image;
 }
 
 export default function ImageCard({ value }: ImageCardProps) {
-  return <img src={value.url} draggable={false} />;
+  return (
+    <img
+      src={value.url}
+      draggable={false}
+      className={classNames({
+        'w-16': value.size === 'small',
+        'w-32': value.size === 'medium',
+        'w-44': value.size === 'large',
+        'object-cover': value.size === 'stretch',
+      })}
+    />
+  );
 }
