2b4e69dec3b21f1518d8ae1a51379a104524a4d3
Alex Acebo <aacebowork@gmail.com>
2025-01-17 12:28:07 -0800

    add blank template

--- packages/cli/src/commands/new/index.ts
+++ packages/cli/src/commands/new/index.ts
@@ -1,4 +1,9 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
 import { CommandModule } from 'yargs';
+
+import * as templates from './templates';
 
 interface Args {
   readonly name: string;
@@ -8,13 +13,23 @@ export const New: CommandModule<{}, Args> = {
   command: 'new <name>',
   aliases: 'n',
   describe: 'create a new app project',
-  builder: (b) => b.positional('name', {
-    alias: 'n',
-    type: 'string',
-    describe: 'the apps name',
-    demandOption: true
-  }),
-  handler: async (args) => {
-    console.log(args);
+  builder: (b) => {
+    return b.positional('name', {
+      alias: 'n',
+      type: 'string',
+      describe: 'the apps name',
+      demandOption: true
+    }).check(({ name }) => {
+      if (fs.existsSync(path.join(process.cwd(), name))) {
+        throw new Error(`"${name}" already exists!`);
+      }
+
+      return true;
+    });
+  },
+  handler: async ({ name }) => {
+    fs.mkdirSync(path.join(process.cwd(), name));
+    fs.mkdirSync(path.join(process.cwd(), name, 'src'));
+    templates.blank(name);
   }
 };
