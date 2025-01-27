ea88fd76dc1f887ac110a846c9f774ab5d374bd3
Alex Acebo <aacebowork@gmail.com>
2025-01-27 11:48:48 -0800

    Update index.ts

--- apps/copilot/src/index.ts
+++ apps/copilot/src/index.ts
@@ -22,8 +22,7 @@ app.on('message', events.message);
 app.on('dialog.open', events.dialogOpen);
 app.on('message.submit.feedback', () => {
   return {
-    status: 200,
-    body: {},
+    status: 200
   };
 });
 
