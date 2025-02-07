b9a956b22fa8b710511076bdb56d27ae6aa1f54a
aacebo <aacebowork@gmail.com>
2025-02-06 23:47:11 -0500

    stuff

--- packages/client/src/app.ts
+++ packages/client/src/app.ts
@@ -1,9 +1,24 @@
+import * as http from '@teams.sdk/common/http';
+
+import pkg from '../package.json';
+
 export interface AppOptions {}
 
 export class App {
   readonly options: AppOptions;
+  readonly http: http.Client;
 
   constructor(options?: AppOptions) {
     this.options = options || {};
+    this.http = new http.Client({
+      headers: {
+        'User-Agent': `teams[client]/${pkg.version}`,
+      },
+    });
+  }
+
+  async call<T = any>(name: string, ...args: any[]) {
+    const res = await this.http.post<T>(`/api/functions/${name}`, args);
+    return res.data;
   }
 }
