d50827ac9bccba494355fc4b3dd4562ecb16ec43
aacebo <aacebowork@gmail.com>
2025-02-12 20:48:51 -0500

    add devtools to botbuilder sample

--- samples/botbuilder/src/index.ts
+++ samples/botbuilder/src/index.ts
@@ -1,6 +1,7 @@
 import { App } from '@teams.sdk/apps';
 import { ConsoleLogger } from '@teams.sdk/common/logging';
 import { BotBuilderPlugin } from '@teams.sdk/botbuilder';
+import { DevtoolsPlugin } from '@teams.sdk/dev';
 import { TeamsActivityHandler } from 'botbuilder';
 
 export class ActivityHandler extends TeamsActivityHandler {
@@ -16,7 +17,10 @@ export class ActivityHandler extends TeamsActivityHandler {
 const handler = new ActivityHandler();
 const app = new App({
   logger: new ConsoleLogger('@samples/botbuilder', { level: 'debug' }),
-  plugins: [new BotBuilderPlugin({ handler })],
+  plugins: [
+    new BotBuilderPlugin({ handler }),
+    new DevtoolsPlugin()
+  ],
 });
 
 app.on('message', async ({ send }) => {
