0cfee27b6fb4f370e2a111678d9f9182c1dd953b
Corina Gum <>
2025-02-13 10:48:15 -0800

    Clean up NewMessageToolbar

--- packages/devtools2/src/components/Toolbar/NewMessageToolbar.tsx
+++ packages/devtools2/src/components/Toolbar/NewMessageToolbar.tsx
@@ -1,17 +1,16 @@
-import * as React from 'react';
-import { AttachRegular, Send20Regular, Send20Filled } from '@fluentui/react-icons';
+import { FC } from 'react';
+import { AttachRegular, Send20Regular, Send20Filled, bundleIcon } from '@fluentui/react-icons';
 import { mergeClasses, Toolbar, ToolbarButton, ToolbarDivider } from '@fluentui/react-components';
 import type { ToolbarProps } from '@fluentui/react-components';
 import useStyles from './Toolbar.styles';
-import { useState } from 'react';
 
 interface NewMessageToolbarProps extends ToolbarProps {
   onSend: () => void;
 }
+const Send = bundleIcon(Send20Filled, Send20Regular);
 
-const NewMessageToolbar: React.FC<NewMessageToolbarProps> = ({ onSend, ...props }) => {
+const NewMessageToolbar: FC<NewMessageToolbarProps> = ({ onSend, ...props }) => {
   const styles = useStyles();
-  const [isSendHovered, setIsSendHovered] = useState(false);
 
   return (
     <Toolbar aria-label="New message actions" {...props} className={styles.toolbar}>
@@ -25,12 +24,9 @@ const NewMessageToolbar: React.FC<NewMessageToolbarProps> = ({ onSend, ...props 
         data-tid="send-button"
         aria-label="Send message"
         className={mergeClasses(styles.toolbarButton, styles.sendButtonFilled)}
-        onMouseEnter={() => setIsSendHovered(true)}
-        onMouseLeave={() => setIsSendHovered(false)}
         onClick={onSend}
-      >
-        {isSendHovered ? <Send20Filled className={styles.sendButtonFilled} /> : <Send20Regular />}
-      </ToolbarButton>
+        icon={<Send />}
+      />
     </Toolbar>
   );
 };
