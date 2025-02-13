43d09f99a9e6c2715f891429ea6ab0220905b933
Corina Gum <>
2025-02-13 10:50:15 -0800

    Prettier

--- packages/devtools2/vite.config.ts
+++ packages/devtools2/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
