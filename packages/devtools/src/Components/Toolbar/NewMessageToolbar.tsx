a99633c789aff539e4c174470c1ec040dc74ebd0
Corina Gum <>
2025-02-20 12:55:32 -0800

    Fix newMessageToolbar use of icons

--- packages/devtools/src/Components/Toolbar/NewMessageToolbar.tsx
+++ packages/devtools/src/Components/Toolbar/NewMessageToolbar.tsx
@@ -1,33 +1,40 @@
 import { FC } from 'react';
-import { AttachRegular, Send20Regular, Send20Filled, bundleIcon } from '@fluentui/react-icons';
+import {
+  AttachRegular,
+  bundleIcon,
+  FluentIcon,
+  SendFilled,
+  SendRegular,
+} from '@fluentui/react-icons/lib/fonts';
 import { Toolbar, ToolbarButton, ToolbarDivider } from '@fluentui/react-components';
 import type { ToolbarProps } from '@fluentui/react-components';
-import useStyles from './Toolbar.styles';
+import { useClasses } from './Toolbar.styles';
 
 interface NewMessageToolbarProps extends ToolbarProps {
   onSend: () => void;
 }
-const Send = bundleIcon(Send20Filled, Send20Regular);
+
+const Send = bundleIcon(SendFilled as FluentIcon, SendRegular as FluentIcon);
 
 const NewMessageToolbar: FC<NewMessageToolbarProps> = ({ onSend, ...props }) => {
-  const classes = useStyles();
+  const classes = useClasses();
 
   return (
-    <Toolbar aria-label="New message actions" {...props} className={classes.toolbar}>
-      <ToolbarButton
-        aria-label="Attach file"
-        icon={<AttachRegular />}
-        className={classes.toolbarButton}
-      />
-      <ToolbarDivider />
-      <ToolbarButton
-        data-tid="send-button"
-        aria-label="Send message"
-        className={classes.toolbarButton}
-        onClick={onSend}
-        icon={<Send />}
-      />
-    </Toolbar>
+      <Toolbar aria-label="New message actions" {...props} className={classes.toolbar}>
+        <ToolbarButton
+          aria-label="Attach file"
+          icon={<AttachRegular />}
+          className={classes.toolbarButton}
+        />
+        <ToolbarDivider />
+        <ToolbarButton
+          data-tid="send-button"
+          aria-label="Send message"
+          className={classes.toolbarButton}
+          onClick={onSend}
+          icon={<Send />}
+        />
+      </Toolbar>
   );
 };
 
