c38256936b46530c03ce51ca8cc9969d06e15c4f
Corina Gum <>
2025-02-21 11:27:10 -0800

    Remove 'minutes ago' part of timestamp to match Teams

--- packages/devtools/src/Utils/date-format.ts
+++ packages/devtools/src/Utils/date-format.ts
@@ -1,20 +1,12 @@
 import {
-  formatDistanceToNow,
   format,
   isToday,
   isYesterday,
   isThisWeek,
-  differenceInHours,
 } from 'date-fns';
 
 export const formatMessageTime = (dateStr: string) => {
   const date = new Date(dateStr);
-  const now = new Date();
-  const hoursDiff = differenceInHours(now, date);
-
-  if (hoursDiff < 1) {
-    return formatDistanceToNow(date, { addSuffix: true });
-  }
 
   if (isToday(date)) {
     return format(date, 'h:mm a');
