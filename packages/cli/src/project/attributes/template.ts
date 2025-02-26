b72cea5ee5c299355e440b8c3bbc644ac570a60e
Alex Acebo <aacebowork@gmail.com>
2025-02-26 11:27:35 -0800

    add `appPackage` to templates

--- packages/cli/src/project/attributes/template.ts
+++ packages/cli/src/project/attributes/template.ts
@@ -36,6 +36,19 @@ export class TemplateAttribute implements ProjectAttribute {
     const pkg = JSON.parse(fs.readFileSync(path.join(templateDir, 'package.json'), 'utf-8'));
     pkg.name = this.name;
     write(templateDir, targetDir, 'package.json', JSON.stringify(pkg, null, 2) + '\n');
+
+    const manifest = JSON.parse(
+      fs.readFileSync(path.join(targetDir, 'appPackage', 'manifest.json'), 'utf-8')
+    );
+    manifest.name.short = `${this.name}-\${{APP_NAME_SUFFIX}}`;
+    manifest.name.full = this.name;
+
+    write(
+      path.join(templateDir, 'appPackage'),
+      path.join(targetDir, 'appPackage'),
+      'manifest.json',
+      JSON.stringify(manifest, null, 2) + '\n'
+    );
   }
 
   csharp(_: string) {}
