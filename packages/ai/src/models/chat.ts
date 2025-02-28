6602574d3efdbdbb533bd5418f23a9cd4e1bacfb
heyitsaamir <ajawaid191@gmail.com>
2025-02-27 17:32:07 -0800

    Fix

--- packages/ai/src/models/chat.ts
+++ packages/ai/src/models/chat.ts
@@ -7,7 +7,7 @@ export type ChatParams<TExtraParams extends {}> = {
   readonly input: Message;
   readonly messages?: Memory;
   readonly functions?: Record<string, Function>;
-} & Partial<Omit<TExtraParams, 'model' | 'input' | 'messages' | 'functions'>>;
+} & Partial<Omit<TExtraParams, 'system' | 'input' | 'messages' | 'functions'>>;
 
 export interface ChatModel<TExtraParams extends {} = {}> {
   chat(
